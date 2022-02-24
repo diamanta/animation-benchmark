c3fedaed0a624bc474f8da6f4770cf512a8d6f46
Valentine Egorov <vvyego15@mts.ru>
2022-02-24 17:13:17 +0300

    More shapes

--- src/app/three/three.component.ts
+++ src/app/three/three.component.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import * as THREE from 'three';
-import { PerspectiveCamera } from 'three';
 
 @Component({
   selector: 'app-three',
@@ -9,11 +8,8 @@ import { PerspectiveCamera } from 'three';
 })
 export class ThreeComponent implements OnInit {
 
-  readonly camera = new THREE.OrthographicCamera( innerWidth / - 2, innerWidth / 2, innerHeight / 2, innerHeight / - 2, 1, 1000 );
+  readonly camera = new THREE.OrthographicCamera(innerWidth / -2, innerWidth / 2, innerHeight / 2, innerHeight / -2, 1, 1000);
   readonly scene = new THREE.Scene();
-  readonly geometry = new THREE.PlaneGeometry(100, 100);
-  readonly material = new THREE.MeshNormalMaterial();
-  readonly mesh = new THREE.Mesh(this.geometry, this.material);
   readonly renderer = new THREE.WebGLRenderer({antialias: true});
 
   constructor(private elementRef: ElementRef, private renderer2: Renderer2) {
@@ -21,13 +17,22 @@ export class ThreeComponent implements OnInit {
 
   ngOnInit(): void {
     this.camera.position.z = 1;
-    this.scene.add(this.mesh);
+    for (let i = 0; i < 5; i++) {
+      this.scene.add(ThreeComponent.meshBuilder(i));
+    }
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.setAnimationLoop(this.animation.bind(this));
     this.renderer2.appendChild(this.elementRef.nativeElement, this.renderer.domElement);
   }
 
-  animation(time: number): void {
+  private animation(time: number): void {
     this.renderer.render(this.scene, this.camera);
+  }
+
+  private static meshBuilder(offset = 0): THREE.Mesh {
+    const dim = 200;
+    const geometry = new THREE.PlaneGeometry(dim, dim * 2);
+    geometry.translate((dim + 50) * offset , dim, 0);
+    return new THREE.Mesh(geometry);
   }
 }
